46fb405ec9e8e72d73548fea902968e10a2f7f98
Juan Pablo Peñaloza Botero <juan-penaloza@javeriana.edu.co>
2017-01-22 18:16:49 -0500

    Added officers page

--- js/script.js
+++ js/script.js
@@ -1,31 +1,22 @@
-// (function(window, document, undefined) {
-//   $(document).ready(function() {
-//     $('body').keyup(function(event) {
-//       if (event.keyCode === 65) {
-//         window.location = '/about';
-//       } else if (event.keyCode === 72) {
-//         window.location = '/';
-//       } else if (event.keyCode === 83) {
-//         window.location = '/sponsors'
-//       }
-//     });
-//
-//     var path = window.location.pathname;
-//     if (path === '/' || path === '/index.html') {
-//       $('.sidebar-left .links a[name="home"]').toggleClass('selected');
-//     } else if (path === '/about/') {
-//       $('.sidebar-left .links a[name="about"]').toggleClass('selected');
-//     } else if (path === '/contact/') {
-//       $('.sidebar-left .links a[name="contact"]').toggleClass('selected');
-//     } else if (path === '/officers/') {
-//       $('.sidebar-left .links a[name="officers"]').toggleClass('selected');
-//     } else if (path === '/sponsors/') {
-//       $('.sidebar-left .links a[name="sponsors"]').toggleClass('selected');
-//     }
-//   });
-//
-//   $(document).mousemove(function(e) {
-//     $('svg.gear').css('transform', 'rotate('+(((e.pageX/5)%360)+((e.pageY/5)%360))+'deg)');
-//   });
-//
-// })(this, this.document);
+(function(window, document, undefined) {
+  $(document).ready(function() {
+
+    var path = window.location.pathname;
+    if (path === '/' || path === '/index.html') {
+      $('.sidebar-left .links a[name="home"]').toggleClass('selected');
+    } else if (path === '/about/') {
+      $('.sidebar-left .links a[name="about"]').toggleClass('selected');
+    } else if (path === '/contact/') {
+      $('.sidebar-left .links a[name="contact"]').toggleClass('selected');
+    } else if (path === '/officers/') {
+      $('.sidebar-left .links a[name="officers"]').toggleClass('selected');
+    } else if (path === '/sponsors/') {
+      $('.sidebar-left .links a[name="sponsors"]').toggleClass('selected');
+    }
+  });
+
+  $(document).mousemove(function(e) {
+    $('svg.gear').css('transform', 'rotate('+(((e.pageX/5)%360)+((e.pageY/5)%360))+'deg)');
+  });
+
+})(this, this.document);
